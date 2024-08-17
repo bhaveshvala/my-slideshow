dc4419471216e6024ab26f5de491fae17eb68857
unknown <bhaveshvala2010@gmail.com>
2024-08-17 19:33:07 +0530

    Added uninstall.php

---
+++ uninstall.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Fired when the plugin is uninstalled.
+ * If uninstall not called from WordPress, then exit.
+ * @since      1.0.0
+ */
+if (!defined('WP_UNINSTALL_PLUGIN')) {
+    exit;
+}
