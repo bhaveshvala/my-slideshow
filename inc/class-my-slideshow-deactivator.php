2fed60307e999d3e231e9516142049fa62062a99
unknown <bhaveshvala2010@gmail.com>
2024-08-17 19:39:12 +0530

    Worked on Class file for plugin deactivator

---
+++ inc/class-my-slideshow-deactivator.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Fired during plugin deactivate.
+ *
+ * This class defines all code necessary to run during the plugin's deactivate.
+ *
+ * @since      1.0.0
+ * @author     Bhavesh Vala
+ */
+class My_Slideshow_Deactivator
+{
+    /**
+     * This run during the plugin's deactivate
+     * @since    1.0.0
+     */
+    public static function deactivate()
+    {
+
+    }
+}
